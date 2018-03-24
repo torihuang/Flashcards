fe3284ac6cef0df0e7c52604d7a3b96f9f5a8a72
Tori Huang <deploy@deploy.com>
2018-03-24 10:46:29 -0500

    Mark flashcards as complete with done button / Reset to see all again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Card from './components/FlipCard';
 import Images from './assets/images';
@@ -10,6 +9,7 @@ const flashcards = [
     frontText: 'Front of card 1',
     backText: 'Back of card 1',
     isFlipped: false,
+    isVisible: true,
   },
   {
     _id: '2',
@@ -17,12 +17,14 @@ const flashcards = [
     backType: 'Image',
     backImage: Images.coffee,
     isFlipped: false,
+    isVisible: true,
   },
   {
     _id: '3',
     frontText: 'Front of card 3',
     backText: 'Café au lait crema so cup est single shot acerbic. Saucer as, black crema organic single origin mocha. Half and half as iced caffeine robusta wings instant. Caramelization brewed con panna, aftertaste, seasonal, froth and, a medium ristretto caramelization caffeine. Mocha crema, lungo, bar, roast in coffee that latte as grinder latte. Cortado, acerbic, grounds coffee doppio brewed sweet. Id, plunger pot single shot, filter, galão spoon blue mountain aged beans. As whipped et chicory aftertaste java robusta est half and half.',
     isFlipped: false,
+    isVisible: true,
   },
 ]
 
@@ -36,26 +38,42 @@ class App extends Component {
 
   // Toggle card isFlipped state
   flipCard(cardId) {
-    console.log('cardId', cardId);
     const newCardStates = this.state.flashcards
     const indexOfFlippedCard = newCardStates.map(card => card._id).indexOf(cardId)
-    console.log('indexOfFlippedCard', indexOfFlippedCard);
     newCardStates[indexOfFlippedCard].isFlipped = !newCardStates[indexOfFlippedCard].isFlipped
-    console.log('newCardStates', newCardStates);
+    this.setState(newCardStates)
+  }
+
+  // Mark card as complete and remove from visible set
+  markAsComplete(cardId) {
+    const newCardStates = this.state.flashcards
+    const indexOfCard = newCardStates.map(card => card._id).indexOf(cardId)
+    newCardStates[indexOfCard].isVisible = false
     this.setState(newCardStates)
   }
 
   // Render all flashcards
   renderFlashcards() {
     return this.state.flashcards.map((flashcard, index) => {
+      if (!flashcard.isVisible) return null
       return (
         <Card
           key={flashcard._id}
           isFirst={index === 0}
           flipCard={this.flipCard.bind(this)}
-          flashcard={flashcard} />
+          flashcard={flashcard}
+          markAsComplete={this.markAsComplete.bind(this)} />
       )
     })
+  }
+
+  // Reset all flashcards to be visible aka isVisible is true
+  resetFlashcards() {
+    const newCardStates = this.state.flashcards.map((flashcard) => {
+      flashcard.isVisible = true
+      return flashcard
+    })
+    this.setState(newCardStates)
   }
 
   render() {
@@ -63,6 +81,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Flashcards</h1>
+          <a className='reset-button' onClick={this.resetFlashcards.bind(this)}>RESET</a>
         </header>
         <div className="flashcard-container">
           {this.renderFlashcards()}
